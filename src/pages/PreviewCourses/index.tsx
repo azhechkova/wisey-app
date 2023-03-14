1a8ca2084e4f692629cdd05dfe849dffdabeca6a
azhechkova <alinazhech@gmail.com>
2023-03-14 23:07:15 +0200

    Config mui and theme

---
+++ src/pages/PreviewCourses/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PreviewCourses = (): React.ReactElement => <div>
+    Courses
+</div>;
+
+export default PreviewCourses;
---
+++ src/theme/index.ts
@@ -0,0 +1,22 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2EC4B6',
+      light: '#9FEBE3',
+      dark: '#1DC4B3',
+    },
+    secondary: {
+      main: '#9FEBE3',
+    },
+    error: {
+      main: '#E71D36',
+    },
+    warning: {
+      main: '#FF9F1C',
+    },
+  },
+});
+
+export default theme;
